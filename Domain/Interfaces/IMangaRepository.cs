fc42db10ae40957566170c40a97a51b0c93f2aca
GustavoMarrufo <gustavitotlgac@gmail.com>
2025-06-19 12:37:51 -0600

    Gustavo modificó el IMangaRepository.cs

--- Domain/Interfaces/IMangaRepository.cs
+++ Domain/Interfaces/IMangaRepository.cs
@@ -1,15 +1,18 @@
 // MiMangaBot/Domain/Interfaces/IMangaRepository.cs
-using MiMangaBot.Domain.Entities; // Asegúrate de usar el namespace correcto de tu entidad Manga
+using MiMangaBot.Domain.Entities;
+using MiMangaBot.Domain.Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace MiMangaBot.Domain.Interfaces; // El namespace debería ser el de tu proyecto + .Domain.Interfaces
-
-public interface IMangaRepository
+namespace MiMangaBot.Domain.Interfaces
 {
-    Task<IEnumerable<Manga>> GetAllAsync();
-    Task<Manga?> GetByIdAsync(string id);
-    Task<Manga> AddAsync(Manga manga);
-    Task<bool> UpdateAsync(Manga manga);
-    Task<bool> DeleteAsync(string id);
+    public interface IMangaRepository
+    {
+        Task<IEnumerable<Manga>> GetAllAsync();
+        Task<PagedResult<Manga>> GetPagedAsync(PaginationParameters paginationParameters);
+        Task<Manga?> GetByIdAsync(string id);
+        Task<Manga> AddAsync(Manga manga);
+        Task<bool> UpdateAsync(Manga manga);
+        Task<bool> DeleteAsync(string id);
+    }
 }
